d4271252d7172d68607af33641159ce0475328c2
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2024-10-20 21:23:26 +0100

    feat: build project layout and design to the fullest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,144 @@
 import React, { useState } from 'react';
+import './App.css';
 import Display from './Display';
-import ButtonContainer from './ButtonContainer';
 
-const Calculator = () => {
-  const [input] = useState('');
-
-  const handleButtonClick = 'value';
+const ThemeProvider = ({ children }) => {
+  const [theme] = useState('theme');
 
   return (
-    <div className="mx-auto mt-[10vh] flex w-[300px] max-w-md flex-col rounded-lg p-4">
-      <Display input={input} />
-      <ButtonContainer onButtonClick={handleButtonClick} />
+    <div className={`main-container ${theme}`}>
+      <header className="header relative my-[50px] mb-[10px] flex">
+        <h1 className="header-text absolute left-[12px] top-[-10px] font-spartan text-[#e9e2db]">
+          calc
+        </h1>
+        <h2 className="header-txt relative left-[150px] top-[-5px] self-center text-[14px] text-[#e9e2db] ">
+          THEME
+        </h2>
+        <div className="theme-container">
+          <form name="themeForm">
+            <div className="theme-label relative left-[94px] top-3 font-spartan text-sm">
+              <label
+                className="theme-1-label theme-label pointer relative left-[70px] top-[-35px] ml-[12px] font-spartan text-[13px] text-[#e9e2db]"
+                htmlFor="theme-1-attr"
+              >
+                1
+              </label>
+              <label
+                className="theme-2-label theme-label pointer relative left-[70px] top-[-35px] ml-[12px] font-spartan text-[13px] text-[#e9e2db]"
+                htmlFor="theme-2-attr"
+              >
+                2
+              </label>
+              <label
+                className="theme-3-label theme-label pointer relative left-[70px] top-[-35px] ml-[12px] font-spartan text-[13px] text-[#e9e2db]"
+                htmlFor="theme-3-attr"
+              >
+                3
+              </label>
+            </div>
+            <div className="theme-input relative left-[166px] top-[-22px] h-[30px] w-[80px] rounded-full bg-[#232c42]">
+              <input
+                className="theme-choice appearance pointer absolute left-[10px] top-[7px] h-[15px] w-[15px] cursor-pointer appearance-none rounded-full"
+                type="radio"
+                name="theme"
+                id="theme-1-attr"
+                value="theme"
+                checked={theme === 'theme'}
+                aria-label="theme-1"
+              />
+            </div>
+          </form>
+        </div>
+      </header>
+      {children}
     </div>
   );
 };
 
-export default Calculator;
+const Calculator = () => {
+  const [result] = useState('0');
+
+  const buttons = [
+    '7',
+    '8',
+    '9',
+    'DEL',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    '.',
+    '0',
+    '/',
+    'x',
+    'RESET',
+    '=',
+  ];
+
+  return (
+    <div className="calculator-container">
+      <Display result={result} />
+
+      <div className="keypad calc align grid rounded-lg bg-[#1A2B4D] px-[10px] py-[15px]">
+        {buttons.map((value) => (
+          <button
+            id="keypad-button"
+            key={value}
+            className={`key ${value === 'RESET' ? 'reset' : ''} ${
+              value === '=' ? 'equals-key equal' : ''
+            } mx-[8px] my-[6px] mb-3 rounded-md border-none bg-[#e9e2db] py-2 font-spartan text-[#434a5a] shadow-[0_5px_#b4a497] active:translate-y-[2px]`}
+            value={value}
+            type="button"
+            style={{
+              ...(value === 'DEL'
+                ? {
+                    backgroundColor: '#687B8F',
+                    fontSize: '20px',
+                    color: '#ffffff',
+                    boxShadow: '0 5px #3D4E66',
+                    transform: "isActive ? 'translateY(2px)' : 'none'",
+                  }
+                : {}),
+              ...(value === 'RESET'
+                ? {
+                    backgroundColor: '#687B8F',
+                    fontWeight: 'bold',
+                    color: '#ffffff',
+                    boxShadow: '0 5px #3D4E66',
+                    fontSize: '18px',
+                    padding: '17px 0',
+                    transform: "isActive ? 'translateY(2px)' : 'none'",
+                  }
+                : {}),
+              ...(value === '='
+                ? {
+                    backgroundColor: '#d6453d',
+                    fontWeight: 'bold',
+                    boxShadow: '0 5px #b02c24',
+                    color: '#ffffff',
+                    transform: "isActive ? 'translateY(2px)' : 'none'",
+                  }
+                : {}),
+            }}
+          >
+            {value}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Calculator />
+    </ThemeProvider>
+  );
+};
+
+export default App;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Display = ({ result }) => {
   return (
     <div className="mb-4 h-[80px] w-[100%] justify-around rounded-lg bg-[#181f34] p-4 pr-2.5 text-right text-white">
-      <div className="text-xl">{'0'}</div>
-      <div className="text-2xl font-bold">{result}</div>
+      <div className="font-spartan text-[28px] font-bold">{result}</div>
     </div>
   );
 };
